f36a6025773b90915e99fcecb67be27738d0c046
Alexander <Alexander@172.16.30.91>
2024-11-14 11:19:57 -0600

    Profe he tenido muchos problemas con mi computadora, realizo este commit desde la computadora de mi trabajo. Se realiza lo visto en clase sobre seguridad en la semana 9

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -7,7 +7,7 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="categoria")
+@Table(name = "categoria")
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -21,9 +21,10 @@ public class Categoria implements Serializable {
     private String rutaImagen;
     private boolean activo;
 
-     @OneToMany
-      @JoinColumn(name="id_categoria")
-     List<Producto> productos;
+    @OneToMany
+    @JoinColumn(name = "id_categoria")
+    List<Producto> productos;
+
     public Categoria() {
     }
 
