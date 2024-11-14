6d0b2fbe8f3fde7a74b35283c8ed4368374e37e1
Alexander <Alexander@172.16.30.91>
2024-11-14 08:54:25 -0600

    Profe he tenido muchos problemas con mi computadora, realizo este commit desde la computadora de mi trabajo. Se realiza lo visto de producto y asociasiones de semana 8.

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -2,6 +2,7 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data
@@ -15,14 +16,14 @@ public class Categoria implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_Categoria")
     private long idCategoria;
-
     private String descripcion;
-    
     @Column(name = "ruta_imagen")
     private String rutaImagen;
-    
     private boolean activo;
 
+     @OneToMany
+      @JoinColumn(name="id_categoria")
+     List<Producto> productos;
     public Categoria() {
     }
 
